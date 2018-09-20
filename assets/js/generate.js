a4e1f9aaeab025a39e749c9dd77009e5f0008818
Herisatry Lubaba <herisatry@gmail.com>
2018-09-20 13:02:46 +0300

    first commit version

---
+++ assets/js/generate.js
@@ -0,0 +1,13 @@
+function randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+    }
+    return pass;
+}
+
+function generate() {
+    myform.row_password.value = randomPassword(myform.length.value);
+}
